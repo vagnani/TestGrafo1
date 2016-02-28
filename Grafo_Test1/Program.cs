cea737286e09ecd4be900eba5b9d866bb4a40c0e
vagnani <vagnani01@gmail.com>
2016-02-28 21:16:26 +0100

    bugfix

--- Grafo_Test1/Program.cs
+++ Grafo_Test1/Program.cs
@@ -24,22 +24,6 @@ namespace MyLibrary.Collections.Grafo
             Console.WriteLine("(nodoPadre,nodoFiglio,valoreNumericoIntero)");
             string str = Console.ReadLine();
             link.AddString(str);
-            //line to insert
-            //(n1,n2,10)(n1,n3,20)(n2,n4,40)(n3,n5,50)(n4,n5,10)(n5,n4,10)(n4,nF,50)(n5,nF,10)
-
-            //MyLinkedListNode n2 = new MyLinkedListNode("n2");
-            //MyLinkedListNode n3 = new MyLinkedListNode("n3");
-            //MyLinkedListNode n4 = new MyLinkedListNode("n4");
-            //MyLinkedListNode n5 = new MyLinkedListNode("n5");
-
-            //link.Add(n2);
-            //link.Add(n3);
-            //link.AddAfter(n2, n4);
-            //link.AddAfter(n3, n5);
-            //link.AddAfter(n4, n5);
-            //link.AddAfter(n5, n4);
-            //link.AddAfter(n4, link._goal);
-            //link.AddAfter(n5, link._goal);
 
 
             foreach (var list in link)
